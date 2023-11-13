1555d0080452aae33fac26c751bb7c871993a35e
GabYrrog <112761786+GabYrrog@users.noreply.github.com>
2023-11-13 13:07:06 -0500

    Added the Employee/Salary school project

---
+++ Employee_Salary/src/validation/ValidationFormulaire.java
@@ -0,0 +1,73 @@
+package validation;
+
+public class ValidationFormulaire {
+
+    private static boolean validerNom(String nom){
+        boolean reponse = true;
+        int spaceNumber = 0;
+        for (int i = 0; i < nom.length(); i++){
+           char currentChar = nom.charAt(i);
+           if (currentChar == ' '){
+                spaceNumber++;
+           }
+
+            if (!Character.isLetter(currentChar) && currentChar != '-' && currentChar != ' ') {
+                reponse = false;
+            }
+            if (spaceNumber > 1){
+                reponse = false;
+            }
+        }
+        return reponse;
+    }
+
+    private static boolean validerNumero(String num){
+        boolean reponse = true;
+        for (int i = 0; i < num.length(); i++) {
+            char currentChar = num.charAt(i);
+            if (!Character.isLetter(currentChar) && !Character.isDigit(currentChar)) {
+                reponse = false;
+            }
+        }
+        return reponse;
+    }
+
+    private static boolean validerSalaireHoraire(double salaire){
+        boolean reponse = true;
+        if (salaire < 15.25){
+            reponse = false;
+        }
+        return reponse;
+    }
+
+    private static boolean validerNbreHeuresSemaine(double nbHeures){
+        boolean reponse = true;
+        if (nbHeures < 0 || nbHeures > 80){
+            reponse = false;
+        }
+        return reponse;
+    }
+
+    private static boolean validerAnciennete(int nbAnnees){
+        boolean reponse = true;
+        if (nbAnnees < 0){
+            reponse = false;
+        }
+        return reponse;
+    }
+
+    public static boolean validerEmploye(String nom, String numero, double salaire, double heuresSem, int anciennete){
+        boolean reponse = true;
+        boolean nomValide = validerNom(nom);
+        boolean numeroValide = validerNumero(numero);
+        boolean salaireValide = validerSalaireHoraire(salaire);
+        boolean heuresSemValide = validerNbreHeuresSemaine(heuresSem);
+        boolean ancienneteValide = validerAnciennete(anciennete);
+
+        if (!nomValide || !numeroValide || !salaireValide || !heuresSemValide || !heuresSemValide || !ancienneteValide){
+            reponse = false;
+        }
+        return reponse;
+    }
+
+}
